a2db1b35de36ede40f2f2cc20e912363fe46f73f
EOLindsay <eorleans_lindsay@st.ug.edu.gh>
2024-06-17 22:10:28 +0000

    Abstract Classes and Methods

--- Console.cs
+++ Console.cs
@@ -1,5 +1,6 @@
 using System;
 
+// Inheritance and Method Overriding
 class Animal
 {
     public virtual void MakeSound()
@@ -7,7 +8,7 @@ class Animal
         Console.WriteLine("Some generic sound");
     }
 }
-
+// 1st derived class
 class Dog : Animal
 {
     public override void MakeSound()
@@ -15,7 +16,7 @@ class Dog : Animal
         Console.WriteLine("Bark");
     }
 }
-
+// 2nd derived class
 class Cat : Animal
 {
     public override void MakeSound()
@@ -23,10 +24,46 @@ class Cat : Animal
         Console.WriteLine("Meow");
     }
 }
+
+// Abstract Classes and Methods
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+// 1st derived class
+class Circle : Shape
+{
+    private double radius;
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+    public override double GetArea()
+    {
+        return Math.PI * radius * radius;
+    }
+}
+// 2nd derived class
+class Rectangle : Shape
+{
+    private double length;
+    private double width;
+    
+    public Rectangle(double length, double width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+    public override double GetArea()
+    {
+        return length * width;
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
+        // Inheritance and Method Overriding
         Animal animal = new Animal();
         Dog dog = new Dog();
         Cat cat = new Cat();
@@ -34,5 +71,16 @@ class Program
         animal.MakeSound();
         dog.MakeSound();
         cat.MakeSound();
+
+        Console.WriteLine();
+
+        // Abstract Classes and Methods
+        Circle circle = new Circle(7.0);
+        Rectangle rectangle = new Rectangle(5.0, 7.0);
+
+        Console.WriteLine($"Circle area: {circle.GetArea()}");
+        Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+
+        Console.WriteLine();
     }
 }
